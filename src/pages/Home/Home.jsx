76660256e05e7b7f2f2b91fbb869b38abf74014f
alexialang <alexialang2@gmail.com>
2024-01-26 10:32:02 +0100

    button download/ responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,7 +17,6 @@ const Home = () => {
         </div>
       </div>
       <div className={classes.aPropos}>
-        <div></div>
         <div >
           <h2>A propos</h2>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, nesciunt totam? Ex officiis voluptas fuga explicabo velit. Sapiente commodi exercitationem distinctio praesentium, accusamus qui consectetur voluptatum unde omnis quo quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id deleniti quos aliquam porro! Ad, adipisci? Sint, expedita! Soluta, ratione ullam! Numquam laudantium non accusamus nostrum necessitatibus consequuntur, reiciendis quod at! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, ea sunt! Saepe ullam enim porro exercitationem delectus magni consectetur repellendus quasi temporibus modi error rem officiis, dignissimos numquam magnam atque.</p>
@@ -31,29 +30,32 @@ const Home = () => {
           <div>
               <h3>HTML</h3>
               <Progress className={classes.bar} color="rgba(255, 175, 105, 1)" value={90} animated /> <br/>
-              <h3>HTML</h3>
-              <Progress className={classes.bar} color="rgba(255, 175, 105, 1)" value={40} animated /> <br/>
-              <h3>HTML</h3>
-              <Progress className={classes.bar} color="rgba(255, 175, 105, 1)" value={50} animated /> <br/>
-              <h3>HTML</h3>
-              <Progress className={classes.bar} color="rgba(255, 175, 105, 1)" value={50} animated /> <br/>
-              <h3>HTML</h3>
-              <Progress className={classes.bar} color="rgba(255, 175, 105, 1)" value={50} animated /> <br/>
-              <h3>HTML</h3>
+              <h3>CSS</h3>
+              <Progress className={classes.bar} color="rgba(255, 175, 105, 1)" value={80} animated /> <br/>
+              <h3>Javascript</h3>
+              <Progress className={classes.bar} color="rgba(255, 175, 105, 1)" value={10} animated /> <br/>
+              <h3>React</h3>
+              <Progress className={classes.bar} color="rgba(255, 175, 105, 1)" value={30} animated /> <br/>
+              <h3>PHP</h3>
+              <Progress className={classes.bar} color="rgba(255, 175, 105, 1)" value={10} animated /> <br/>
+              <h3>SQL</h3>
               <Progress className={classes.bar}color="rgba(255, 175, 105, 1)" value={50} animated /> <br/>
-              <h3>HTML</h3>
-              <Progress color="rgba(255, 175, 105, 1)" value={50} animated /> <br/>
+              <h3>Figma</h3>
+              <Progress color="rgba(255, 175, 105, 1)" value={60} animated /> <br/>
           </div>
           <div>
             <img src="../../src/assets/cv.png"></img>
-            <Button variant="filled" color="orange">Télécharger mon cv</Button>
+            <a href="fichier.html" download="../../src/assets/cv.png">
+            <Button variant="filled" color="orange" >Télécharger mon cv</Button>
+  </a>
+           
            </div>
         </div>
       </div>
       <div className={classes.projets}>
         <h2>Mes projets</h2>
         <div className={classes.blockProjets}>
-          <div className={classes.coucou}><img src="../../src/assets/cv.png"></img> <p>coucou</p> </div>
+          <div><img src="../../src/assets/cv.png"></img> <p>coucou</p> </div>
           <div><img src="../../src/assets/cv.png"></img><p>coucou</p></div>
           <div><img src="../../src/assets/cv.png"></img><p>coucou</p></div>
         </div>
